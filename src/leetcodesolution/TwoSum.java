fce33cc979b20e971f8da3f5c1eae47fa1d0edbd
rutvij.kokne@gmail.com <kokne@DESKTOP-BNJR65D>
2023-01-31 23:38:09 +0530

    New Comment1

--- src/leetcodesolution/TwoSum.java
+++ src/leetcodesolution/TwoSum.java
@@ -10,7 +10,7 @@ public class TwoSum {
         }
     }
 
-    public static int[] twoSumArray(int[] nums, int target) { 
+    public static int[] twoSumArray(int[] nums, int target) {
         int[] aa = new int[2];
         for (int i = 0; i < nums.length ; i++) {
             for (int j = i + 1; j < nums.length; j++) {
