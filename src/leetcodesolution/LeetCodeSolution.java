08e30f25928dd0b4d5674d6d32d2e27577917dfc
sanketkambli09@gmail.com <>
2023-01-31 14:51:24 +0530

    Test

--- src/leetcodesolution/LeetCodeSolution.java
+++ src/leetcodesolution/LeetCodeSolution.java
@@ -16,6 +16,9 @@ public class LeetCodeSolution {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        int i = 0; 
+        System.out.println("Hello World");
     }
     
 }
