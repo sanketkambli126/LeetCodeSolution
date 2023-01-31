a9d126342d4055557a14160763da736828dda2a5
rutvij.kokne@gmail.com <kokne@DESKTOP-BNJR65D>
2023-01-31 18:31:23 +0530

    Adding PSVM

--- src/leetcodesolution/TwoSum.java
+++ src/leetcodesolution/TwoSum.java
@@ -10,7 +10,27 @@ package leetcodesolution;
  * @author kokne
  */
 public class TwoSum {
+
     public static void main(String[] args) {
-        
+        int a[] = {2, 7, 11, 15};
+        int[] a111 = twoSumArray(a, 9);
+        for (int i : a111) {
+            System.out.println(i);
+        }
+    }
+
+    public static int[] twoSumArray(int[] nums, int target) {
+        int[] aa = new int[2];
+        for (int i = 0; i < nums.length ; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    aa[0] = i;
+                    aa[1] = j;
+                    return aa;
+                }
+            }
+        }
+        return aa;
     }
 }
+
