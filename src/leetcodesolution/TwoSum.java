c494c752f4dfce81ba61f5fa3a2b018423c5b7da
rutvij.kokne@gmail.com <kokne@DESKTOP-BNJR65D>
2023-01-31 16:53:58 +0530

    Adding PSVM

--- src/leetcodesolution/TwoSum.java
+++ src/leetcodesolution/TwoSum.java
@@ -1,9 +1,16 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package leetcodesolution;
 
-
+/**
+ *
+ * @author kokne
+ */
 public class TwoSum {
     public static void main(String[] args) {
-        int a[]={1,2,3}; 
+        
     }
 }
