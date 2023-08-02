ce6dae44f0e98c2d416e843a66693065e5eb6b9a
Rutvij Kokne <124033074+rutvijkokne@users.noreply.github.com>
2023-08-02 09:32:05 +0530

    Commit Chnages
    Commit Chnages

--- src/leetcodesolution/FourNumberSum.java
+++ src/leetcodesolution/FourNumberSum.java
@@ -1,16 +1,29 @@
 package leetcodesolution;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FourNumberSum {
 
     public static List<Integer[]> fourNumberSum(int[] array, int targetSum) {
-        // Write your code here.
-        return new ArrayList<Integer[]>();
+        List<Integer[]> result = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                for (int k = j + 1; k < array.length; k++) {
+                    for (int l = k + 1; l < array.length; l++) {
+                        if (array[i] + array[j] + array[k] + array[l] == targetSum) {
+                            result.add(new Integer[]{array[i], array[j], array[k], array[l]});
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(fourNumberSum(new int[]{7, 6, 4, -1, 1, 2}, 16));
+        System.out.println(Arrays.deepToString(fourNumberSum(new int[]{7, 6, 4, -1, 1, 2}, 16).toArray()));
     }
 }
